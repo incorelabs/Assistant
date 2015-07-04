f918599d1c5be63d4cacc6ef8221c7bb46bcd6be
Darshan <darshan@Darshans-MacBook-Pro.local>
2015-07-04 12:37:36 +0530

    Modified the A-Z scroller

--- contacts/index.php
+++ contacts/index.php
@@ -273,35 +273,39 @@
     <div class="row">
 
       <!-- A-Z Picker -->
-      <div class="col-md-1 col-sm-2 col-xs-2">
-          <ul class="affix">
-            <li><a href="">A</a></li>
-            <li><a href="">B</a></li>
-            <li><a href="">C</a></li>
-            <li><a href="">D</a></li>
-            <li><a href="">E</a></li>
-            <li><a href="">F</a></li>
-            <li><a href="">G</a></li>
-            <li><a href="">H</a></li>
-            <li><a href="">I</a></li>
-            <li><a href="">J</a></li>
-            <li><a href="">K</a></li>
-            <li><a href="">L</a></li>
-            <li><a href="">M</a></li> 
-            <li><a href="">N</a></li> 
-            <li><a href="">O</a></li> 
-            <li><a href="">P</a></li> 
-            <li><a href="">Q</a></li> 
-            <li><a href="">R</a></li> 
-            <li><a href="">S</a></li> 
-            <li><a href="">T</a></li> 
-            <li><a href="">U</a></li> 
-            <li><a href="">V</a></li> 
-            <li><a href="">W</a></li> 
-            <li><a href="">X</a></li> 
-            <li><a href="">Y</a></li> 
-            <li><a href="">Z</a></li>
-          </ul>
+      <div class="col-md-1 col-sm-2 col-xs-2 ul_margin">
+          <nav>
+            <ul class="affix">
+              <center>
+                <li class="ul_pad"><a href="#">A</a></li>
+                <li class="ul_pad"><a href="#">B</a></li>
+                <li class="ul_pad"><a href="#">C</a></li>
+                <li class="ul_pad"><a href="#">D</a></li>
+                <li class="ul_pad"><a href="#">E</a></li>
+                <li class="ul_pad"><a href="#">F</a></li>
+                <li class="ul_pad"><a href="#">G</a></li>
+                <li class="ul_pad"><a href="#">H</a></li>
+                <li class="ul_pad"><a href="#">I</a></li>
+                <li class="ul_pad"><a href="#">J</a></li>
+                <li class="ul_pad"><a href="#">K</a></li>
+                <li class="ul_pad"><a href="#">L</a></li>
+                <li class="ul_pad"><a href="#">M</a></li>
+                <li class="ul_pad"><a href="#">N</a></li>
+                <li class="ul_pad"><a href="#">O</a></li>
+                <li class="ul_pad"><a href="#">P</a></li>
+                <li class="ul_pad"><a href="#">Q</a></li>
+                <li class="ul_pad"><a href="#">R</a></li>
+                <li class="ul_pad"><a href="#">S</a></li>
+                <li class="ul_pad"><a href="#">T</a></li>
+                <li class="ul_pad"><a href="#">U</a></li>
+                <li class="ul_pad"><a href="#">V</a></li>
+                <li class="ul_pad"><a href="#">W</a></li>
+                <li class="ul_pad"><a href="#">X</a></li>
+                <li class="ul_pad"><a href="#">Y</a></li>
+                <li class="ul_pad"><a href="#">Z</a></li>
+              </center>
+            </ul>
+          </nav>
       </div>
 
 
