d758be3933bc09f492d86677b7059163268485bb
Darshan <darshan@Darshans-MacBook-Pro.local>
2015-08-14 18:43:48 +0530

    Script.js Final

--- dist/script/script.js
+++ dist/script/script.js
@@ -1,45 +1,15 @@
 //var root = "http://incorelabs.com/Assistant/";
 var root = "http://localhost/Assist/";
-$(document).ready(function(){
-	$(".logout").click(function(event){
- 
-  		$.ajax({
-		    method: "GET",
-		    url: root+"api/logout.php",
-		  })
-		    .done(function(msg) {
-		      var response = JSON.parse(msg);
-		      if (response.status == 1) {
-		      	window.location.href = "index.php";
-		      }
-		      else{
-		      	showNotificationFailure("Logout unsuccessful. Please try again");
-		      }
-		    });
-  	});
+var errorStatus = [false,false,false,false,false,false,false];
+var email_count = 0;
+var pwd_count = 0;
+var c_pwd_count = 0;
+var name_count = 0;
+var country_count = 0;
+var mobile_count = 0;
+var dob_count = 0;
 
-  	var errorStatus = [false,false,false,false,false,false,false];
-	var email_count = 0;
-	var pwd_count = 0;
-	var c_pwd_count = 0;
-	var name_count = 0;
-	var country_count = 0;
-	var mobile_count = 0;
-	var dob_count = 0;
-
-	$(".email").focusout(function(){
-		validateEmail(this);
-	});
-
-	$(".email").focusin(function()
-	{
-		var email = $(this).val();
-		var formGroup = $(this).closest(".form-group");
-		formGroup.removeClass("has-error");
-		$(this).closest('.form-group').find('.info').empty();
-	});
-
-	function validateEmail(element){
+function validateEmail(element){
 		var email = $(element).val();
 		var formGroup = $(element).closest(".form-group");
 		var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
@@ -75,6 +45,262 @@ $(document).ready(function(){
 		}
 	}
 
+function validatePassword(element){
+	var password = $(element).val();
+	var formGroup = $(element).closest(".form-group");
+	var letter = /[a-zA-Z]/;
+	var number = /[0-9]/;
+	var msg = "";
+
+		if(password.length == 0)
+	{
+		formGroup.removeClass("has-success");
+		formGroup.addClass("has-error");
+		$(element).closest('.form-group').find('.info').html("Please Enter a Password");
+		pwd_count = 0
+		errorStatus[1] = false;
+	}
+	
+	else
+	{
+		if(password.length < 8 || password.length >16)
+		{
+			formGroup.removeClass("has-success");
+			formGroup.addClass("has-error");
+			$(element).closest('.form-group').find('.info').html("Password should between 8 to 16 characters");
+			pwd_count = 0;
+			errorStatus[1] = false;
+		}
+
+		else
+		{
+			if ( number.test(password) && letter.test(password) )
+			{
+				formGroup.addClass("has-success");
+				formGroup.removeClass("has-error");
+				$(element).closest('.form-group').find('.info').empty();
+				pwd_count = 1;	
+				errorStatus[1] = true;
+			}
+			else
+			{
+				formGroup.removeClass("has-success");
+				formGroup.addClass("has-error");
+				$(element).closest('.form-group').find('.info').html("Password should atleast have 1 alphabet and one number");
+				pwd_count = 0;	
+				errorStatus[1] = false;
+			}
+		}	
+	}
+}
+
+function validateConfirmPassword(p,element)
+{
+	var password = $(p).val();
+	var c_password = $(element).val();
+	var formGroup = $(element).closest(".form-group");
+	if(c_password.length == 0 || c_password.length == "")
+	{
+		formGroup.removeClass("has-success");
+		formGroup.addClass("has-error");
+		$(element).closest('.form-group').find('.info').html("Please Enter Confirm Password");
+		c_pwd_count = 0;
+		errorStatus[2] = false;
+	}
+	else
+	{
+		if(c_password == password)
+		{
+			formGroup.addClass("has-success");
+			formGroup.removeClass("has-error");
+			$(element).closest('.form-group').find('.info').empty();
+			c_pwd_count = 1;
+			errorStatus[2] = true;
+		}
+		else
+		{
+			formGroup.removeClass("has-success");
+			formGroup.addClass("has-error");
+			$(element).closest('.form-group').find('.info').html("Passwords do not match");
+			c_pwd_count = 0;
+			errorStatus[2] = false;
+		}
+	}
+}
+
+function validateName(element){
+	var name = $(element).val();
+	var formGroup = $(element).closest(".form-group");
+	if (name.length == 0 || name.length == "")
+	{
+		formGroup.removeClass("has-success");
+		formGroup.addClass("has-error");
+		$(element).closest('.form-group').find('.info').html("Please Enter a Name");
+		name_count = 0;
+		errorStatus[3] = false;
+	}
+	else
+	{
+		if (name.length < 3 || name.length > 40) 
+		{
+			formGroup.addClass("has-error");
+			formGroup.removeClass("has-success");
+			$(element).closest('.form-group').find('.info').html("Please Enter a Valid Name");
+			name_count = 0;
+			errorStatus[3] = false;
+		}
+		else
+		{
+			if(!name.match(/^[a-zA-Z\s]+$/))
+			{
+				formGroup.addClass("has-error");
+				formGroup.removeClass("has-success");
+				$(element).closest('.form-group').find('.info').html("Name must consist of alphabets only");
+				name_count = 0;
+				errorStatus[3] = false;
+			}
+			else
+			{
+				formGroup.removeClass("has-error");
+				formGroup.addClass("has-success");
+				$(element).closest('.form-group').find('.info').empty();
+				name_count = 1;
+				errorStatus[3] = true;
+			}
+		}
+	}
+}
+
+function validateCountry(element)
+{
+	var country = $(element).val();
+	var formGroup = $(element).closest(".form-group");
+	if (country == "") 
+	{
+		formGroup.removeClass("has-success");
+		formGroup.addClass("has-error");
+		$(element).closest('.form-group').find('.info').html("Select a Country");
+		country_count = 0;
+		errorStatus[4] = false;
+	}
+	else
+	{
+		formGroup.removeClass("has-error");
+		formGroup.addClass("has-success");
+		$(element).closest('.form-group').find('.info').empty();
+		country_count = 1;
+		errorStatus[4] = true;
+	}
+}
+
+function validateMobile(element)
+{
+	var mobile = $(element).val();
+	var indNum = /^[0]?[56789]\d{9}$/;
+	var country = $('#country').val();
+	var formGroup = $(element).closest(".form-group");
+	if (country == "")
+	{
+		formGroup.removeClass("has-success");
+			formGroup.addClass("has-error");
+			$(element).closest('.form-group').find('.info').html("Please Select a country");
+			mobile_count = 0;
+			errorStatus[5] = false;
+	}
+	else
+	{
+		if (mobile.length != 10 || isNaN(mobile))
+		{
+				formGroup.removeClass("has-success");
+				formGroup.addClass("has-error");
+				$(element).closest('.form-group').find('.info').html("Please Enter a Valid Phone Number");
+				mobile_count = 0;
+				errorStatus[5] = false;
+		}
+		else
+		{
+			if(country == 1046)
+			{
+				if(indNum.test(mobile))
+				{
+					formGroup.removeClass("has-error");
+					formGroup.addClass("has-success");
+					$(element).closest('.form-group').find('.info').empty();
+					mobile_count = 1;
+					errorStatus[5] = true;
+				}
+				else
+				{
+					formGroup.removeClass("has-success");
+					formGroup.addClass("has-error");
+					$(element).closest('.form-group').find('.info').html("Please Enter a Valid Phone Number");
+					mobile_count = 0;
+					errorStatus[5] = false;
+				}
+			}
+			else
+			{
+				formGroup.removeClass("has-error");
+				formGroup.addClass("has-success");
+				$(element).closest('.form-group').find('.info').empty();
+				mobile_count = 1;
+				errorStatus[5] = true;
+			}
+		}
+	}
+}
+
+function validateDate(element){
+	var formGroup = $(element).closest(".form-group");
+	var date = new MyDate($(element).val());
+	if (date.isValid()){
+		formGroup.removeClass("has-error");
+		formGroup.addClass("has-success");
+		$(element).closest('.form-group').find('.info').empty();
+		dob_count = 1;
+		errorStatus[6] = true;
+	}
+	else{
+		formGroup.removeClass("has-success");
+		formGroup.addClass("has-error");
+		$(element).closest('.form-group').find('.info').html("Invalid Date. Enter Date in dd/mm/yyyy format");
+		dob_count = 0;
+		errorStatus[6] = false;
+	}
+}
+
+$(document).ready(function()
+{
+	$(".logout").click(function(event){
+ 
+  		$.ajax({
+		    method: "GET",
+		    url: root+"api/logout.php",
+		  })
+		    .done(function(msg) {
+		      var response = JSON.parse(msg);
+		      if (response.status == 1) {
+		      	window.location.href = "index.php";
+		      }
+		      else{
+		      	showNotificationFailure("Logout unsuccessful. Please try again");
+		      }
+		    });
+  	});
+
+	$(".email").focusout(function(){
+		validateEmail(this);
+	});
+
+	$(".email").focusin(function()
+	{
+		var email = $(this).val();
+		var formGroup = $(this).closest(".form-group");
+		formGroup.removeClass("has-error");
+		$(this).closest('.form-group').find('.info').empty();
+	});
+
+	
 	$(".password").focusout(function(){
 		validatePassword(this);
 	});
@@ -86,55 +312,6 @@ $(document).ready(function(){
 		formGroup.removeClass("has-error");
 		$(this).closest('.form-group').find('.info').empty();
 	});
-
-	function validatePassword(element){
-		var password = $(element).val();
-		var formGroup = $(element).closest(".form-group");
-		var letter = /[a-zA-Z]/;
-    	var number = /[0-9]/;
-		var msg = "";
-
-  		if(password.length == 0)
-		{
-			formGroup.removeClass("has-success");
-			formGroup.addClass("has-error");
-			$(element).closest('.form-group').find('.info').html("Please Enter a Password");
-			pwd_count = 0
-			errorStatus[1] = false;
-		}
-		
-		else
-		{
-			if(password.length < 8 || password.length >16)
-			{
-				formGroup.removeClass("has-success");
-				formGroup.addClass("has-error");
-				$(element).closest('.form-group').find('.info').html("Password should between 8 to 16 characters");
-				pwd_count = 0;
-				errorStatus[1] = false;
-			}
-
-			else
-			{
-				if ( number.test(password) && letter.test(password) )
-				{
-					formGroup.addClass("has-success");
-					formGroup.removeClass("has-error");
-					$(element).closest('.form-group').find('.info').empty();
-					pwd_count = 1;	
-					errorStatus[1] = true;
-				}
-				else
-				{
-					formGroup.removeClass("has-success");
-					formGroup.addClass("has-error");
-					$(element).closest('.form-group').find('.info').html("Password should atleast have 1 alphabet and one number");
-					pwd_count = 0;	
-					errorStatus[1] = false;
-				}
-			}	
-		}
-	}
 
 	$(".c_password").focusout(function(){
 		
@@ -148,39 +325,6 @@ $(document).ready(function(){
 		$(this).closest('.form-group').find('.info').empty();
 	});
 
-	function validateConfirmPassword(p,element){
-		var password = $(p).val();
-		var c_password = $(element).val();
-		var formGroup = $(element).closest(".form-group");
-		if(c_password.length == 0 || c_password.length == "")
-		{
-			formGroup.removeClass("has-success");
-			formGroup.addClass("has-error");
-			$(element).closest('.form-group').find('.info').html("Please Enter Confirm Password");
-			c_pwd_count = 0;
-			errorStatus[2] = false;
-		}
-		else
-		{
-			if(c_password == password)
-			{
-				formGroup.addClass("has-success");
-				formGroup.removeClass("has-error");
-				$(element).closest('.form-group').find('.info').empty();
-				c_pwd_count = 1;
-				errorStatus[2] = true;
-			}
-			else
-			{
-				formGroup.removeClass("has-success");
-				formGroup.addClass("has-error");
-				$(element).closest('.form-group').find('.info').html("Passwords do not match");
-				c_pwd_count = 0;
-				errorStatus[2] = false;
-			}
-		}
-	}
-
 	$(".name").focusout(function(){
 		validateName(this);
 	});
@@ -191,49 +335,6 @@ $(document).ready(function(){
 		formGroup.removeClass("has-error");
 		$(this).closest('.form-group').find('.info').empty();
 	});
-
-	function validateName(element){
-		var name = $(element).val();
-		var formGroup = $(element).closest(".form-group");
-		if (name.length == 0 || name.length == "")
-		{
-			formGroup.removeClass("has-success");
-			formGroup.addClass("has-error");
-			$(element).closest('.form-group').find('.info').html("Please Enter a Name");
-			name_count = 0;
-			errorStatus[3] = false;
-		}
-		else
-		{
-			if (name.length < 3 || name.length > 40) 
-			{
-				formGroup.addClass("has-error");
-				formGroup.removeClass("has-success");
-				$(element).closest('.form-group').find('.info').html("Please Enter a Valid Name");
-				name_count = 0;
-				errorStatus[3] = false;
-			}
-			else
-			{
-				if(!name.match(/^[a-zA-Z\s]+$/))
-				{
-					formGroup.addClass("has-error");
-					formGroup.removeClass("has-success");
-					$(element).closest('.form-group').find('.info').html("Name must consist of alphabets only");
-					name_count = 0;
-					errorStatus[3] = false;
-				}
-				else
-				{
-					formGroup.removeClass("has-error");
-					formGroup.addClass("has-success");
-					$(element).closest('.form-group').find('.info').empty();
-					name_count = 1;
-					errorStatus[3] = true;
-				}
-			}
-		}
-	}
 
 	$("#country").focusout(function(){
 		validateCountry(this);
@@ -247,26 +348,7 @@ $(document).ready(function(){
 		$(this).closest('.form-group').find('.info').empty();
 	});
 
-	function validateCountry(element){
-		var country = $(element).val();
-		var formGroup = $(element).closest(".form-group");
-		if (country == "") 
-		{
-			formGroup.removeClass("has-success");
-			formGroup.addClass("has-error");
-			$(element).closest('.form-group').find('.info').html("Select a Country");
-			country_count = 0;
-			errorStatus[4] = false;
-		}
-		else
-		{
-			formGroup.removeClass("has-error");
-			formGroup.addClass("has-success");
-			$(element).closest('.form-group').find('.info').empty();
-			country_count = 1;
-			errorStatus[4] = true;
-		}
-	}
+	
 
 	$(".mobile").focusout(function(){
 		validateMobile(this);
@@ -280,61 +362,7 @@ $(document).ready(function(){
 		$(this).closest('.form-group').find('.info').empty();
 	});
 
-	function validateMobile(element){
-		var mobile = $(element).val();
-		var indNum = /^[0]?[56789]\d{9}$/;
-		var country = $('#country').val();
-		var formGroup = $(element).closest(".form-group");
-		if (country == "")
-		{
-			formGroup.removeClass("has-success");
-				formGroup.addClass("has-error");
-				$(element).closest('.form-group').find('.info').html("Please Select a country");
-				mobile_count = 0;
-				errorStatus[5] = false;
-		}
-		else
-		{
-			if (mobile.length != 10 || isNaN(mobile))
-			{
-					formGroup.removeClass("has-success");
-					formGroup.addClass("has-error");
-					$(element).closest('.form-group').find('.info').html("Please Enter a Valid Phone Number");
-					mobile_count = 0;
-					errorStatus[5] = false;
-			}
-			else
-			{
-				if(country == 1046)
-				{
-					if(indNum.test(mobile))
-					{
-						formGroup.removeClass("has-error");
-						formGroup.addClass("has-success");
-						$(element).closest('.form-group').find('.info').empty();
-						mobile_count = 1;
-						errorStatus[5] = true;
-					}
-					else
-					{
-						formGroup.removeClass("has-success");
-						formGroup.addClass("has-error");
-						$(element).closest('.form-group').find('.info').html("Please Enter a Valid Phone Number");
-						mobile_count = 0;
-						errorStatus[5] = false;
-					}
-				}
-				else
-				{
-					formGroup.removeClass("has-error");
-					formGroup.addClass("has-success");
-					$(element).closest('.form-group').find('.info').empty();
-					mobile_count = 1;
-					errorStatus[5] = true;
-				}
-			}
-		}
-	}
+	
 
 	$(".date").focusout(function(){
 		validateDate(this);
@@ -347,25 +375,6 @@ $(document).ready(function(){
 		formGroup.removeClass("has-error");
 		$(this).closest('.form-group').find('.info').empty();
 	});
-
-	function validateDate(element){
-		var formGroup = $(element).closest(".form-group");
-		var date = new MyDate($(element).val());
-		if (date.isValid()){
-			formGroup.removeClass("has-error");
-			formGroup.addClass("has-success");
-			$(element).closest('.form-group').find('.info').empty();
-			dob_count = 1;
-			errorStatus[6] = true;
-		}
-		else{
-			formGroup.removeClass("has-success");
-			formGroup.addClass("has-error");
-			$(element).closest('.form-group').find('.info').html("Invalid Date. Enter Date in dd/mm/yyyy format");
-			dob_count = 0;
-			errorStatus[6] = false;
-		}
-	}
 });
 
 function showNotificationSuccess(msg) 
