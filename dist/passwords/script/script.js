b26d65ae9d674e889b07ac9324c065946f092614
Neelabh Pandey <incore.labs.np@gmail.com>
2015-08-30 11:53:13 +0530

    Revert "better mobile view"
    This reverts commit 9e5613c91682d6b22000dd88c238d3a59b5b8d21.

--- dist/passwords/script/script.js
+++ dist/passwords/script/script.js
@@ -76,7 +76,7 @@ function setPasswordDetail(index){
 		$("#passwordDetailsHeader").removeClass('hidden-xs hidden-sm');
 
 		//Show the Password Details and hides the password list
-		$("#passwordListDiv").addClass('hidden');
+		$("#passwordList").addClass('hidden');
 		$("#passwordDetails").removeClass('hidden-xs hidden-sm');
 
 		//Show Hide of menu button with back button
@@ -89,14 +89,14 @@ function setPasswordDetail(index){
 			$("#searchPasswordHeader").removeClass('hidden');
 
 			//Show the Password Details and hides the password list
-			$("#passwordListDiv").removeClass('hidden');
+			$("#passwordList").removeClass('hidden');
 			$("#passwordDetails").addClass('hidden-xs hidden-sm');
 
 			//Show Hide of menu button with back button
 			$(".menu_img").removeClass('hidden');
 			$("#backButton").addClass('hidden');
-
 		});
+
 	}
 	if(passwordList.length == 0)
 	{
@@ -314,18 +314,4 @@ $(document).ready(function() {
 	      	}
 	    }
 	});
-
-	if(window.innerWidth < 992)
-	{
-		$("body").css("overflow", "auto");
-		$("#passwordListScroll").removeClass("scroll");
-		$("#passwordsList").addClass("mobile-list");
-		$("#passwordListDiv").addClass("mobileBody");
-		$("#searchPasswordHeader").addClass("mobileHeader");
-
-		$("#password-Detail").removeClass("scroll");
-		$("#passwordDetails").addClass("mobileBody");
-		$("#passwordDetailsHeader").addClass("mobileHeader");
-
-	}
 });
