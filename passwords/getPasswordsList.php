0d2cc4884e543eff1b495e5c0e0aeadd95306135
Kamlesh Bokdia <kbokdia@gmail.com>
2015-08-31 14:20:21 +0530

    Retrieve Single Password added

--- passwords/getPasswordsList.php
+++ passwords/getPasswordsList.php
@@ -14,10 +14,10 @@ $response = array();
 function getPasswordList($orderBy){
 	global $response,$mysqli,$regCode;
 
-	$sql = "SELECT Table152.`RegCode`, Table152.`PasswordCode`, Table152.`PasswordTypeCode`, Table130.PasswordTypeName, Table152.`HolderCode`, Table107.FamilyName as 'HolderName',Table152.`PasswordName`, Table152.`LoginID`, Table152.`LoginPassword1`, Table152.`LoginPassword2`, Table152.`InsertedBy`, Table152.`PrivateFlag`, Table152.`ActiveFlag` FROM `Table152` 
-		INNER JOIN Table130 ON Table130.PasswordTypeCode = Table152.`PasswordTypeCode` 
-		INNER JOIN Table107 ON Table107.RegCode = Table152.`RegCode` AND Table107.FamilyCode = Table152.`HolderCode` 
-		WHERE Table152.`RegCode` = ".$regCode."
+	$sql = "SELECT Table152.RegCode, Table152.PasswordCode, Table152.PasswordTypeCode, Table130.PasswordTypeName, Table152.HolderCode, Table107.FamilyName as 'HolderName',Table152.PasswordName, Table152.LoginID, Table152.LoginPassword1, Table152.LoginPassword2, Table152.InsertedBy, Table152.PrivateFlag, Table152.ActiveFlag FROM Table152
+		INNER JOIN Table130 ON Table130.PasswordTypeCode = Table152.PasswordTypeCode
+		INNER JOIN Table107 ON Table107.RegCode = Table152.RegCode AND Table107.FamilyCode = Table152.HolderCode
+		WHERE Table152.RegCode = ".$regCode."
 		ORDER BY ".$orderBy.";";
 	//echo $sql;
 	if ($result = $mysqli->query($sql)) {
@@ -29,10 +29,20 @@ function getPasswordList($orderBy){
 	}
 }
 
-if (isset($_GET['list'])) {
-	$listType = intval($_GET['list']);
-	getPasswordList("Table107.FamilyName");
-}
+do{
+	if (isset($_GET['list'])) {
+		$listType = intval($_GET['list']);
+		getPasswordList("Table107.FamilyName");
+	}
+
+	if(!empty($_GET['passwordCode'])){
+        $passwordCode = $_GET['passwordCode'];
+		$sql = "SELECT Table152.RegCode, Table152.PasswordCode, Table152.PasswordTypeCode, Table130.PasswordTypeName, Table152.HolderCode, Table107.FamilyName as 'HolderName',Table152.PasswordName, Table152.LoginID, Table152.LoginPassword1, Table152.LoginPassword2, Table152.InsertedBy, Table152.PrivateFlag, Table152.ActiveFlag FROM Table152 INNER JOIN Table130 ON Table130.PasswordTypeCode = Table152.PasswordTypeCode LEFT JOIN Table107 ON Table107.RegCode = Table152.RegCode AND Table107.FamilyCode = Table152.HolderCode WHERE Table152.RegCode = ".$regCode." AND Table152.PasswordCode = ".$passwordCode."  ORDER BY FamilyName  LIMIT 1;";
+		if($result = $mysqli->query($sql)){
+            $response = $result->fetch_assoc();
+        }
+	}
+}while(0);
 
 echo json_encode($response);
 $mysqli->close();
