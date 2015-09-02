52c9e3c19ed39e3b1a112da1a5dbf9a2d098ebee
Neelabh Pandey <incore.labs.np@gmail.com>
2015-09-02 15:50:50 +0530

    Fixes to the comments in the CDN repositories.

--- dist/bootstrap.php
+++ dist/bootstrap.php
@@ -2,14 +2,20 @@
 
 <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">-->
 <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">-->
-<!--    <!-- Optional theme -->-->
+
+<!-- Optional theme -->
 <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">-->
-<!--    <!--jQuery UI-->-->
+
+<!--jQuery UI-->
 <!--<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.min.css">-->
-<!--	<!--Adding jquery file-->-->
+
+<!--Adding jquery file-->
+
 <!--<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>-->
 <!--<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>-->
-<!--    <!-- Latest compiled and minified JavaScript -->-->
+
+<!-- Latest compiled and minified JavaScript -->
+
 <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>-->
 
 <!-- Non Minified files -->
