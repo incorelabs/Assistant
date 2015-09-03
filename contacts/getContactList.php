2e9b27b7bc513f3a48fefc1d5d7d7760aec362dd
Kamlesh Bokdia <kbokdia@gmail.com>
2015-09-03 17:47:33 +0530

    Contact List api Release 2

--- contacts/getContactList.php
+++ contacts/getContactList.php
@@ -4,28 +4,70 @@ define("ROOT", "../");
 include_once ROOT.'dist/authenticate.php';
 require_once ROOT.'db/Connection.php';
 
-$mysqli = getConnection();
-$regCode = intval($_SESSION['s_id']);
-
 class ContactList{
     var $limit;
+    var $pages;
     var $requestPage;
     var $contactList;
     var $count;
     var $mysqli;
     var $regCode;
+    var $response;
+    var $successful; // if contacts retrieved then successful
 
     function __construct($limit,$page){
         $this->limit = $limit;
         $this->requestPage = $page;
         $this->regCode = intval($_SESSION['s_id']);
         $this->mysqli = getConnection();
+        $this->setCount();
     }
 
     function getLimits(){
         $lower = $this->requestPage * $this->limit;
         $upper = $lower + $this->limit - 1;
         return array("lower"=>$lower, "upper" => $upper);
+    }
+
+    function getContactListQuery(){
+        $sql = "SELECT Table151.ContactCode,
+                Table151.FullName
+            FROM Table151
+            ORDER BY Table151.FullName
+            LIMIT ".$this->limit." OFFSET ".$this->getLimits()["lower"].";";
+        return $sql;
+    }
+
+    function getResponse(){
+        if($this->successful){
+            $this->setPages();
+            $this->response = createResponse(1,"Success");
+            $this->response["pages"] = $this->pages;
+            $this->response["result"] = $this->contactList;
+        }
+        else{
+            $this->response = createResponse(0,"No contacts");
+        }
+
+        return $this->response;
+    }
+
+    function setContactList(){
+        $sql = $this->getContactListQuery();
+        if($result = $this->mysqli->query($sql)){
+
+            if($result->num_rows == 0){
+                $this->successful = false;
+            }
+            else{
+                $i = 0;
+                while($row = $result->fetch_assoc()){
+                    $this->contactList[$i] = array($row['ContactCode'],$row['FullName']);
+                    $i++;
+                }
+                $this->successful = true;
+            }
+        }
     }
 
     function setMysqli($mysqli){
@@ -48,9 +90,27 @@ class ContactList{
         if(is_null($count)){
             $sql = "SELECT count(*) as 'count' FROM Table151 WHERE Table151.RegCode = ".$this->regCode;
             if($result = $this->mysqli->query($sql)) {
-
+                $row = $result->fetch_assoc();
+                $this->count = intval($row['count']);
+                $this->setContactList();
             }
         }
+        else{
+            $this->count = $count;
+        }
+    }
+
+    function setPages($pages=null){
+        if(is_null($pages)){
+            $this->pages = intval($this->count / $this->limit + 1);
+        }
+        else{
+            $this->pages = $pages;
+        }
+    }
+
+    function __destruct(){
+        $this->mysqli->close();
     }
 }
 
@@ -63,7 +123,7 @@ $response;
 
 //General validation
 do{
-    if(isset($_GET['pageNo'])){
+    if(!empty($_GET['pageNo'])){
         $validate = true;
     }
     else{
@@ -74,48 +134,11 @@ do{
 }while(0);
 
 if($validate){
+    $limit = 3; //should be greater than 1
+    $requestPage = intval($_GET['pageNo']) - 1;
 
-    $limit = 250;
-    $requestPage = intval($_GET['pageNo']);
-
-    $logicObj = new ContactList($limit,$requestPage);
-    $contactList = array();
-
-    $sql = "SELECT Table151.ContactCode,
-                Table151.FullName
-            FROM Table151
-            ORDER BY Table151.FullName
-            LIMIT ".$limit." OFFSET ".$logicObj->getLimits()["lower"].";";
-
-    //echo $sql;
-    if($result = $mysqli->query($sql)){
-
-        if($result->num_rows == 0){
-            $validate = false;
-            $response = createResponse(0,"No contacts");
-        }
-        else{
-            $i = 0;
-            while($row = $result->fetch_assoc()){
-                $contactList[$i] = array($row['ContactCode'],$row['FullName']);
-                $i++;
-            }
-            $validate = true;
-        }
-    }
-
-    if($validate){
-        $sql = "SELECT count(*) as 'count' FROM Table151 WHERE Table151.RegCode = ".$regCode;
-        if($result = $mysqli->query($sql)){
-            $row = $result->fetch_assoc();
-            $noOfContacts = intval($row['count']);
-            $pages = round($noOfContacts/$limit);
-            $response = createResponse(1,"Success");
-            $response["pages"] = $pages;
-            $response["result"] = $contactList;
-            $validate = true;
-        }
-    }
+    $contactListObj = new ContactList($limit,$requestPage);
+    $response = $contactListObj->getResponse();
 }
 
 echo json_encode($response);
