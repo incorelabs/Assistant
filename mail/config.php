cc78a6abb2098a6b31ce8df38afc612b123e2023
Kamlesh Bokdia <kbokdia@gmail.com>
2015-09-14 14:07:09 +0530

    Removing Mail folder from git

--- mail/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	define("MAIL_HOST", "vps.allinoneassistant.net");
-	define("MAIL_USERNAME", "kbokdia@allinoneassistant.net");
-	define("MAIL_PASSWORD", "kamlesh@123");
-	define("MAIL_PORT", "465");
-	define("MAIL_FROM", "no-reply@allinoneassistant.com");
-	define("MAIL_FROM_NAME", "Assistant");
-?>
