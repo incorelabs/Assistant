09b925b08c8a5be1ea10a8786d93201f9c5c57a7
Kamlesh Bokdia <kbokdia@gmail.com>
2015-09-15 18:33:58 +0530

    Envelope Controller added.

--- preferences/envelopeSettings/EnvelopeSettings.php
+++ preferences/envelopeSettings/EnvelopeSettings.php
@@ -16,15 +16,19 @@ class EnvelopeSettings
     var $regCode;
     var $familyCode;
     var $response;
+    var $data;
+    var $mode;
+    var $where;
 
     function __construct(){
         $this->regCode = intval($_SESSION['s_id']);
         $this->familyCode = intval($_SESSION['familyCode']);
         $this->mysqli = getConnection();
+        $this->where = " WHERE RegCode = ".$this->regCode;
     }
 
     function getSelectQuery(){
-        return "SELECT * FROM TABLE135";
+        return "SELECT * FROM TABLE135".$this->where;
     }
 
     function getEnvelopeList(){
@@ -41,6 +45,132 @@ class EnvelopeSettings
             $response = $this->mysqli->error;
         }
         return $response;
+    }
+
+    function getDeleteQuery(){
+        return "DELETE FROM Table135".$this->where." AND CoverCode = ".$this->data['coverCode'];
+    }
+
+    function deleteEnvelope(){
+        $this->runQuery($this->getDeleteQuery());
+    }
+
+    function getUpdateQuery(){
+        $sql = "UPDATE `Table135` SET `CoverName` = ".$this->data['coverName'].", `FromRequired` = ".$this->data['fromRequired'].", `FromTop` = ".$this->data['fromTop'].", `FromLeft` = ".$this->data['fromLeft'].", `ToTop` = ".$this->data['toTop'].", `ToLeft` = ".$this->data['toLeft'].", `FromName` = ".$this->data['fromName'].", `FromAdd1` = ".$this->data['fromAdd1'].", `FromAdd2` = ".$this->data['fromAdd2'].", `FromAdd3` = ".$this->data['fromAdd3'].", `FromAdd4` = ".$this->data['fromAdd4'].", `LogoAvailable` = ".$this->data['logoAvailable'].", `LogoPath` = ".$this->data['logoPath'].", `LogoTop` = ".$this->data['logoTop'].", `LogoLeft` = ".$this->data['logoLeft'].", `LogoWidth` = ".$this->data['logoWidth'].", `Caption` = ".$this->data['caption'].", `CoverFeed` = ".$this->data['coverFeed']." WHERE `CoverCode` = ".$this->data['coverCode']." AND `RegCode` = ".$this->regCode;
+
+        return $sql;
+    }
+
+    function editEnvelope(){
+        $this->runQuery($this->getUpdateQuery());
+    }
+
+    function getInsertQuery(){
+        $coverCode = $this->generateCoverCode();
+        $sql = "INSERT INTO Table135 (`CoverCode`,`CoverName`,`FromRequired`,`FromTop`,`FromLeft`,`ToTop`,`ToLeft`,`FromName`,`FromAdd1`,`FromAdd2`,`FromAdd3`,`FromAdd4`,`LogoAvailable`,`LogoPath`,`LogoTop`,`LogoLeft`,`LogoWidth`,`Caption`,`CoverFeed`,`RegCode`) VALUES (".$coverCode.",".$this->data['coverName'].",".$this->data['fromRequired'].",".$this->data['fromTop'].",".$this->data['fromLeft'].",".$this->data['toTop'].",".$this->data['toLeft'].",".$this->data['fromName'].",".$this->data['fromAdd1'].",".$this->data['fromAdd2'].",".$this->data['fromAdd3'].",".$this->data['fromAdd4'].",".$this->data['logoAvailable'].",".$this->data['logoPath'].",".$this->data['logoTop'].",".$this->data['logoLeft'].",".$this->data['logoWidth'].",".$this->data['caption'].",".$this->data['coverFeed'].",".$this->regCode.")";
+
+        return $sql;
+    }
+
+    function addEnvelope(){
+        $this->runQuery($this->getInsertQuery());
+    }
+
+    function cleanData(){
+        $this->data['coverName'] = ((!empty($this->data['coverName'])) ? $this->data['coverName'] : NULL);
+        $this->data['fromRequired'] = ((!empty($this->data['fromRequired'])) ? $this->data['fromRequired'] : NULL);
+        $this->data['fromTop'] = ((!empty($this->data['fromTop'])) ? $this->data['fromTop'] : NULL);
+        $this->data['fromLeft'] = ((!empty($this->data['fromLeft'])) ? $this->data['fromLeft'] : NULL);
+        $this->data['toTop'] = ((!empty($this->data['toTop'])) ? $this->data['toTop'] : NULL);
+        $this->data['toLeft'] = ((!empty($this->data['toLeft'])) ? $this->data['toLeft'] : NULL);
+        $this->data['fromName'] = ((!empty($this->data['fromName'])) ? $this->data['fromName'] : NULL);
+        $this->data['fromAdd1'] = ((!empty($this->data['fromAdd1'])) ? $this->data['fromAdd1'] : NULL);
+        $this->data['fromAdd2'] = ((!empty($this->data['fromAdd2'])) ? $this->data['fromAdd2'] : NULL);
+        $this->data['fromAdd3'] = ((!empty($this->data['fromAdd3'])) ? $this->data['fromAdd3'] : NULL);
+        $this->data['fromAdd4'] = ((!empty($this->data['fromAdd4'])) ? $this->data['fromAdd4'] : NULL);
+        $this->data['logoAvailable'] = ((!empty($this->data['logoAvailable'])) ? $this->data['logoAvailable'] : NULL);
+        $this->data['logoPath'] = ((!empty($this->data['logoPath'])) ? $this->data['logoPath'] : NULL);
+        $this->data['logoTop'] = ((!empty($this->data['logoTop'])) ? $this->data['logoTop'] : NULL);
+        $this->data['logoLeft'] = ((!empty($this->data['logoLeft'])) ? $this->data['logoLeft'] : NULL);
+        $this->data['logoWidth'] = ((!empty($this->data['logoWidth'])) ? $this->data['logoWidth'] : NULL);
+        $this->data['caption'] = ((!empty($this->data['caption'])) ? $this->data['caption'] : NULL);
+        $this->data['coverFeed'] = ((!empty($this->data['coverFeed'])) ? $this->data['coverFeed'] : NULL);
+        $this->data['coverCode'] = ((!empty($this->data['coverCode'])) ? $this->data['coverCode'] : NULL);
+
+        foreach ($this->data as $key=>$value) {
+            if(is_numeric($value)){
+                $this->data[$key] = $value;
+            }
+            elseif(is_string($value)){
+                if($value == ''){
+                    $this->data[$key] = "null";
+                }
+                else{
+                    $this->data[$key] = "'".$value."'";
+                }
+            }
+            elseif(is_null($value)){
+                $this->data[$key] = "null";
+            }
+        }
+
+        //print_r($this->data);
+
+    }
+
+    function generateCoverCode(){
+        $coverCode = 1001;
+        $sql = "SELECT MAX(CoverCode) AS 'coverCode' FROM Table135".$this->where;
+        if($result = $this->mysqli->query($sql)){
+            $coverCode = intval($result->fetch_assoc()['coverCode']);
+            $coverCode = (($coverCode == 0) ? 1001 : $coverCode + 1);
+        }
+        else{
+            $this->response = $this->createResponse(0,$this->mysqli->error);
+        }
+
+        return $coverCode;
+    }
+
+    function runQuery($sql){
+        if ($this->mysqli->query($sql) === TRUE) {
+            $this->response = $this->createResponse(1,"Successful");
+        }
+        else{
+            $this->response = $this->createResponse(0,"Error occurred while uploading to the database: ".$this->mysqli->error);
+        }
+    }
+
+    function setData($data){
+        $this->data = $data;
+
+        //set mode
+        if ($this->data["mode"] == "A") {
+            $this->mode = 1;
+        } elseif ($this->data["mode"] == "M") {
+            $this->mode = 2;
+        } elseif ($this->data["mode"] == "D") {
+            $this->mode = 3;
+        }
+
+        $this->cleanData();
+
+        //call respect methods based on mode
+        switch($this->mode){
+            case 1:
+                $this->addEnvelope();
+                break;
+            case 2:
+                $this->editEnvelope();
+                break;
+            case 3:
+                $this->deleteEnvelope();
+                break;
+        }
+    }
+
+    function getResponse(){
+        return $this->response;
     }
 
     function createResponse($status,$message){
---
+++ preferences/envelopeSettings/controller.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kbokdia
+ * Date: 15/09/15
+ * Time: 4:41 PM
+ */
+
+namespace Assistant\Preferences\Envelope;
+require 'EnvelopeAutoload.php';
+
+function createResponse($status,$message){
+    return array('status' => $status, 'message' => $message);
+}
+
+$response = createResponse(0,"Initialize");
+
+//Do validation if required
+do{
+    if (isset($_POST)) {
+        $validate = true;
+    }
+    else{
+        $validate = false;
+        $response = createResponse(0,"Invalid");
+        break;
+    }
+
+    if(empty($_POST['mode']) || empty($_POST["coverCode"]) || empty($_POST["fromRequired"]) || empty($_POST["logoAvailable"]) || empty($_POST["coverFeed"]) || empty($_POST["coverName"]) ) {
+        $validate = false;
+        $response = createResponse(0,"Invalid Request");
+        break;
+    }
+
+    //Delete mode
+    if ($_POST["mode"] == "D") {
+        break;
+    }
+
+    if(intval($_POST["fromRequired"]) == 1){
+        if(empty($_POST["fromTop"]) || empty($_POST["fromLeft"]) || empty($_POST["fromName"])){
+            $validate = false;
+            $response = createResponse(0,"Please add From Top, Left and Name values");
+            break;
+        }
+    }
+
+    if(intval($_POST["logoAvailable"]) == 1){
+        if(empty($_POST["logoTop"]) || empty($_POST["logoLeft"]) || empty($_POST["logoHeight"]) || empty($_POST["logoWidth"])){
+            $validate = false;
+            $response = createResponse(0,"Please add Logo Top, Left, Height and Width values");
+            break;
+        }
+    }
+}while(0);
+
+if($validate){
+    $settings = new EnvelopeSettings();
+    $settings->setData($_POST);
+    $response = $settings->getResponse();
+}
+
+echo json_encode($response);
