1b2fe7c6761a512a5f66c17966daef89f88d843e
Kamlesh Bokdia <kbokdia@gmail.com>
2015-09-16 16:12:20 +0530

    Image upload added to envelope

---
+++ preferences/envelopeSettings/upload.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kbokdia
+ * Date: 16/09/15
+ * Time: 2:56 PM
+ */
+
+namespace Assistant\Preferences\Envelope;
+require 'EnvelopeAutoload.php';
+require ROOT."external/class.upload.php";
+
+function createResponse($status,$message){
+    return array('status' => $status, 'message' => $message);
+}
+
+$response = createResponse(0,"Initialize");
+$validate = true;
+do{
+    if(empty($_POST['coverCode'])){
+        $validate = false;
+        $response = createResponse(0,"Invalid Request");
+    }
+}while(0);
+
+if($validate) {
+    $logo = new \upload($_FILES["fileToUpload"]);
+    if($logo->uploaded){
+        //check upload size
+        $logo->file_max_size = 1024 * 1024; // 1 MB
+
+        //check mime type
+        $logo->mime_check = true;
+        $logo->allowed = array('image/*');
+        $logo->forbidden = array('application/*');
+
+        //upload
+        $logo->file_new_name_body = $_POST["coverCode"];
+        $logo->image_convert = 'jpg';
+        $logo->file_overwrite = true;
+        $logo->process(ROOT."../Assistant_Users/".$_SESSION['s_id']."/preferences/envelope");
+        if($logo->processed){
+            $response = createResponse(1,"Logo uploaded successfully");
+            $logo->clean();
+        }
+        else{
+            $response = createResponse(0,$logo->error);
+        }
+    }
+}
+
+echo json_encode($response);
