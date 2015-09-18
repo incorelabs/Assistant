53700e2eeadf8deefaa91f8f98ac78c280ab54e6
Kamlesh Bokdia <kbokdia@gmail.com>
2015-09-18 12:21:04 +0530

    Image Mapper add to img folder

---
+++ img/getImage.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kbokdia
+ * Date: 18/09/15
+ * Time: 11:44 AM
+ */
+session_start();
+define("ROOT", "../");
+
+require ROOT.'dist/authenticate.php';
+$regCode = intval($_SESSION['s_id']);
+
+$file = urldecode($_GET['file']);
+$fileDir = '../../Assistant_Users/'.$regCode."/";
+
+if (file_exists($fileDir . $file))
+{
+    $fileName = $fileDir . $file;
+
+    $contents = file_get_contents($fileName);
+
+    $fileInfo = new finfo(FILEINFO_MIME_TYPE);
+    $contentType = $fileInfo->file($fileName);
+
+    header("Content-type: ".$contentType);
+
+    echo $contents;
+}
