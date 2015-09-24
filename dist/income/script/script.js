8a603868b614e7278171de7132c43ff4f0285ad0
Neelabh Pandey <incore.labs.np@gmail.com>
2015-09-24 11:09:54 +0530

    income scripts updated.

--- dist/income/script/script.js
+++ dist/income/script/script.js
@@ -3,7 +3,191 @@ var pageIncome = {
     localIncome: null,
     defIncomeList: $.Deferred(),
     defSearchResult: $.Deferred(),
+    familyList: null,
+    incomeList: null,
     firstTime: true,
+    getFamilyList: function () {
+        var url = localStorage.getItem("websiteRoot") + "family/getFamily.php";
+
+        $.getJSON(url, {
+            list: 2
+        }).done(function (data) {
+            pageIncome.setFamilyList(data);
+        }).fail(function (error) {
+
+        });
+    },
+    setFamilyList: function (data) {
+        pageIncome.familyList = data;
+        var familyListString = "";
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].FamilyCode == familyCode) {
+                familyListString += "<option value = " + data[i].FamilyCode + " selected = 'selected'>" + data[i].FamilyName + "</option>";
+            }
+            else {
+                familyListString += "<option value = " + data[i].FamilyCode + ">" + data[i].FamilyName + "</option>";
+            }
+        }
+        $("#holderCode").html(familyListString);
+    },
+    getIncomeList: function () {
+        var url = localStorage.getItem("websiteRoot") + "income/getIncomeList.php";
+
+        $.getJSON(url, {
+            pageNo: pageIncome.currentPageNo
+        }).done(function (data) {
+            pageIncome.defIncomeList.resolve(data);
+            pageIncome.setIncomeList(data);
+        }).fail(function (error) {
+
+        });
+    },
+    setIncomeList: function (data) {
+        if (data.status == 1) {
+            $('#loadMore').remove();
+            pageIncome.currentPageNo++;
+            var incomeListString = "";
+            for (var i = 0; i < data.result.length; i++) {
+                incomeListString += "<a onclick='pageIncome.getIncomeDetails(" + data.result[i].IncomeCode + ")' class='list-group-item contacts_font'>" + data.result[i].IncomeName + "</a>";
+            }
+            $("#incomeList").append(incomeListString);
+            if (pageIncome.currentPageNo <= data.pages) {
+                // Show Load More
+                var loadMoreString = "<div id='loadMore' class='list-group-item' align='center'><a class='list-group-item-text header_font' style='cursor: pointer;' onclick='pageIncome.getIncomeList();'>Load more..</a></div>";
+                $("#incomeList").append(loadMoreString);
+            }
+        } else {
+            var noMoreDataString = "<div class='list-group-item list-border-none'><li class='list-group-item-text header_font'>";
+            noMoreDataString += data.message + "</li></div>";
+            $("#incomeList").empty().html(noMoreDataString);
+        }
+    },
+    doSearch: function () {
+        $("#incomeList").empty();
+        pageIncome.currentPageNo = 1;
+        pageIncome.firstTime = true;
+        pageIncome.defSearchResult = $.Deferred();
+        pageIncome.getSearchResults();
+        $.when(pageIncome.defSearchResult).done(function (data) {
+            if (data.status == 1)
+                pageIncome.getIncomeDetails(data.result[0].IncomeCode);
+        });
+    },
+    getSearchResults: function () {
+        var url = localStorage.getItem("websiteRoot") + "income/getIncomeList.php";
+
+        $.getJSON(url, {
+            pageNo: pageIncome.currentPageNo,
+            searchType: 1,
+            searchText: $('#searchBox').val().trim()
+        }).done(function (data) {
+            pageIncome.defSearchResult.resolve(data);
+            pageIncome.setSearchResults(data);
+        }).fail(function (error) {
+
+        });
+    },
+    setSearchResults: function (data) {
+        if (data.status == 1) {
+            $('#loadMore').remove();
+            pageIncome.currentPageNo++;
+            var searchResultsString = "";
+            for (var i = 0; i < data.result.length; i++) {
+                searchResultsString += "<a onclick='pageIncome.getIncomeDetails(" + data.result[i].IncomeCode + ")' class='list-group-item contacts_font'>" + data.result[i].IncomeName + "</a>";
+            }
+            $("#incomeList").append(searchResultsString);
+            if (pageIncome.currentPageNo <= data.pages) {
+                // Show Load More
+                var loadMoreString = "<div id='loadMore' class='list-group-item' align='center'><a class='list-group-item-text header_font' style='cursor: pointer;' onclick='pageIncome.getSearchResults();'>Load more..</a></div>";
+                $("#incomeList").append(loadMoreString);
+            }
+        } else {
+            var noMoreDataString = "<div class='list-group-item list-border-none'><li class='list-group-item-text header_font'>";
+            noMoreDataString += data.message + "</li></div>";
+            $("#incomeList").empty().html(noMoreDataString);
+            $("#incomeDetailBody").empty();
+            $("#editIncomeBtn").remove();
+            $("#deleteIncomeBtn").remove();
+        }
+    },
+    getIncomeDetails: function (incomeCode) {
+        if (incomeCode == null)
+            return;
+        var url = localStorage.getItem("websiteRoot") + "income/getIncomeDetail.php";
+
+        $.getJSON(url, {
+            incomeCode: incomeCode
+        }).done(function (data) {
+            pageIncome.setIncomeDetails(data);
+        }).fail(function (error) {
+
+        });
+    },
+    setIncomeDetails: function (data) {
+        if (data.status == 1) {
+            pageIncome.localIncome = data.detail;
+
+            var incomeHeaderString = "<h12>Income Details</h12><button id='editIncomeBtn' class='btn btn-success pull-right btn-header-margin-left' onclick='pageIncome.openEditIncomeModal();'><span class='glyphicon glyphicon-pencil'></span></button><button id='deleteIncomeBtn' class='btn btn-danger pull-left' onclick='pageIncome.openDeleteIncomeModal(" + data.detail.income.IncomeCode + ")'><span class='glyphicon glyphicon-trash'></span></button><button id='voucherIncomeBtn' class='btn btn-info pull-right' onclick='pageIncome.openVoucherIncomeModal()'><span class='fa fa-sticky-note-o fa-lg'></span></button>";
+            var incomeDetailString = "";
+            if (window.innerWidth < 992 && !pageIncome.firstTime) {
+                //Show the Income Details Header and hides the search header
+                $("#searchIncomeHeader").addClass('hidden');
+                $("#incomeDetailHeader").removeClass('hidden-xs hidden-sm');
+
+                //Show the Income Details and hides the income list
+                $("#incomeListDiv").addClass('hidden');
+                $("#incomeDetailDiv").removeClass('hidden-xs hidden-sm');
+
+                //Show Hide of menu button with back button
+                $(".menu_img").addClass('hidden');
+                $("#backButton").removeClass('hidden');
+
+                $("#backButton").click(function () {
+                    //Show the Income Details Header and hides the search header
+                    $("#incomeDetailHeader").addClass('hidden-xs hidden-sm');
+                    $("#searchIncomeHeader").removeClass('hidden');
+
+                    //Show the Income Details and hides the income list
+                    $("#incomeListDiv").removeClass('hidden');
+                    $("#incomeDetailDiv").addClass('hidden-xs hidden-sm');
+
+                    //Show Hide of menu button with back button
+                    $(".menu_img").removeClass('hidden');
+                    $("#backButton").addClass('hidden');
+
+                });
+            }
+            pageIncome.firstTime = false;
+
+            incomeDetailString += "<div class='row contact-details row-top-padding'><div class='list-group-item-heading header_font'><div class='col-md-3'>Holder's Name</div><value><div class='col-md-9'>" + ((data.detail.income.HolderName) ? data.detail.income.HolderName : "") + "</div></value></div></div>";
+
+            incomeDetailString += "<div class='row contact-details'><div class='list-group-item-heading header_font'><div class='col-md-3'>Income Type</div><value><div class='col-md-9'>" + ((data.detail.income.IncomeTypeName) ? data.detail.income.IncomeTypeName : "") + "</div></value></div></div>";
+
+            incomeDetailString += "<div class='row contact-details'><div class='list-group-item-heading header_font'><div class='col-md-3'>Description</div><value><div class='col-md-9'>" + ((data.detail.income.IncomeName) ? data.detail.income.IncomeName : "") + "</div></value></div></div>";
+
+            incomeDetailString += "<div class='row contact-details'><div class='list-group-item-heading header_font'><div class='col-md-3'>Due To</div><value><div class='col-md-9'>" + ((data.detail.income.FullName) ? data.detail.income.FullName : "") + "</div></value></div></div>";
+
+            incomeDetailString += "<div class='row contact-details'><div class='list-group-item-heading header_font'><div class='col-md-3'>Joint Holder Name</div><value><div class='col-md-9'>" + ((data.detail.income.JointHolder) ? data.detail.income.JointHolder : "") + "</div></value></div></div>";
+
+            incomeDetailString += "<div class='row contact-details'><div class='list-group-item-heading header_font'><div class='col-md-3'>Remarks</div><value><div class='col-md-9'>" + ((data.detail.income.IncomeRemarks) ? data.detail.income.IncomeRemarks : "") + "</div></value></div></div>";
+
+            incomeDetailString += "<div class='row contact-details'><div class='list-group-item-heading header_font'><div class='col-md-3'>Billing Day</div><value><div class='col-md-9'>" + ((data.detail.income.BillingDay) ? data.detail.income.BillingDay : "") + "</div></value></div></div>";
+
+            incomeDetailString += "<div class='row contact-details'><div class='list-group-item-heading header_font'><div class='col-md-3'>Due Day</div><value><div class='col-md-9'>" + ((data.detail.income.DueDay) ? data.detail.income.DueDay : "") + "</div></value></div></div>";
+
+            incomeDetailString += "<div class='row contact-details'><div class='list-group-item-heading header_font'><div class='col-md-3'>Payment Frequency</div><value><div class='col-md-9'>" + ((data.detail.income.IncomeFrequency) ? data.detail.income.IncomeFrequency : "") + "</div></value></div></div>";
+
+            incomeDetailString += "<div class='row contact-details'><div class='list-group-item-heading header_font'><div class='col-md-3'>Loan Due Date</div><value><div class='col-md-9'>" + ((data.detail.income.ExpiryDate) ? data.detail.income.ExpiryDate : "") + "</div></value></div></div>";
+
+            incomeDetailString += "<div class='row contact-details'><div class='list-group-item-heading header_font'><div class='col-md-3'>Payment URL</div><value><div class='col-md-9'>" + ((data.detail.income.PayWebsite) ? data.detail.income.PayWebsite : "") + "</div></value></div></div>";
+
+            $("#incomeDetailHeader").html(incomeHeaderString);
+            $("#incomeDetailBody").html(incomeDetailString);
+
+        } else {
+            pageIncome.localIncome = null;
+        }
+    },
     openAddIncomeModal: function(){
         document.getElementById("incomeForm").reset();
         $('#addPrivacy').attr('checked', false);
@@ -36,6 +220,41 @@ var pageIncome = {
 };
 
 $(document).ready(function () {
+    localStorage.setItem("websiteRoot", "../");
+
+    document.getElementById('searchBox').onkeypress = function (e) {
+        if (!e)
+            e = window.event;
+        console.log(e);
+        var keyCode = e.keyCode || e.which;
+        if (keyCode == '13') {
+            // Enter pressed
+            pageIncome.doSearch();
+        }
+    };
+
+    $.when(pageIncome.defIncomeList).done(function (data) {
+        if (data.status == 1)
+            pageIncome.getIncomeDetails(data.result[0].IncomeCode);
+    });
+
+    pageIncome.getFamilyList();
+    pageIncome.getIncomeList();
+
+    $("#searchBox").on('input propertychange', function () {
+        if ($(this).val().trim() == "") {
+            $("#incomeList").empty();
+            pageIncome.currentPageNo = 1;
+            pageIncome.firstTime = true;
+            pageIncome.defIncomeList = $.Deferred();
+            pageIncome.getIncomeList();
+            $.when(pageIncome.defIncomeList).done(function (data) {
+                if (data.status == 1)
+                    pageIncome.getIncomeDetails(data.result[0].IncomeCode);
+            });
+        }
+    });
+
     if(window.innerWidth < 992)
     {
         $("#billingDayDiv").removeClass("first-col-left-padding first-col-right-padding");
