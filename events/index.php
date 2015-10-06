c704741285267c3b154b755fecb1b8645fabba3d
darshan <darshanturakhia23@gmail.com>
2015-10-06 19:46:05 +0530

    event css fix

--- events/index.php
+++ events/index.php
@@ -268,66 +268,68 @@ echo $navbar_str;
                 </div>
                 <div class="modal-body">
                     <div class="row">
-                    <div class="col-md-12 col-sm-12 col-xs-12">
-                        <div class="form-group form-group-margin">
-                            <div class="col-md-6 col-sm-6 col-xs-12 multiselect-col-margin">
-                                <div class="input-group">
-                                    <span class="input-group-addon input-group-addon-label">State*</span>
-                                    <select id="statesSelection" multiple="multiple" class="form-control">
-                                        <option value='Andaman and Nicobar Islands'>Andaman and Nicobar</option>
-                                        <option value='Andhra Pradesh'>Andhra Pradesh</option>
-                                        <option value='Arunachal Pradesh'>Arunachal Pradesh</option>
-                                        <option value='Assam'>Assam</option>
-                                        <option value='Bihar'>Bihar</option>
-                                        <option value='Chandigarh'>Chandigarh</option>
-                                        <option value='Chhattisgarh'>Chhattisgarh</option>
-                                        <option value='Dadra and Nagar Haveli'>Dadra and Nagar Haveli</option>
-                                        <option value='Daman and Diu'>Daman and Diu</option>
-                                        <option value='Delhi'>Delhi</option>
-                                        <option value='Goa'>Goa</option>
-                                        <option value='Gujarat'>Gujarat</option>
-                                        <option value='Haryana'>Haryana</option>
-                                        <option value='Himachal Pradesh'>Himachal Pradesh</option>
-                                        <option value='Jammu and Kashmir'>Jammu and Kashmir</option>
-                                        <option value='Jharkhand'>Jharkhand</option>
-                                        <option value='Karnataka'>Karnataka</option>
-                                        <option value='Kerala'>Kerala</option>
-                                        <option value='Lakshadweep'>Lakshadweep</option>
-                                        <option value='Madhya Pradesh'>Madhya Pradesh</option>
-                                        <option value='Maharashtra'>Maharashtra</option>
-                                        <option value='Manipur'>Manipur</option>
-                                        <option value='Meghalaya'>Meghalaya</option>
-                                        <option value='Mizoram'>Mizoram</option>
-                                        <option value='Nagaland'>Nagaland</option>
-                                        <option value='Odisha'>Odisha</option>
-                                        <option value='Puducherry'>Puducherry</option>
-                                        <option value='Punjab'>Punjab</option>
-                                        <option value='Rajasthan'>Rajasthan</option>
-                                        <option value='Sikkim'>Sikkim</option>
-                                        <option value='Tamil Nadu'>Tamil Nadu</option>
-                                        <option value='Telengana'>Telengana</option>
-                                        <option value='Tripura'>Tripura</option>
-                                        <option value='Uttar Pradesh'>Uttar Pradesh</option>
-                                        <option value='Uttarakhand'>Uttarakhand</option>
-                                        <option value='West Bengal'>West Bengal</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-6 col-sm-6 col-xs-12 multiselect-col-margin top-padding">
-                                <div class="input-group">
-                                    <span class="input-group-addon input-group-addon-label">Cities*</span>
-                                    <select id="citiesSelection" multiple="multiple" class="form-control">
-                                        <option value="cheese">Cheese</option>
-                                        <option value="tomatoes">Tomatoes</option>
-                                        <option value="mozarella">Mozzarella</option>
-                                        <option value="mushrooms">Mushrooms</option>
-                                        <option value="pepperoni">Pepperoni</option>
-                                        <option value="onions">Onions</option>
-                                    </select>
-                                </div>
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+                            <div class="form-group form-group-margin">
+                                    <div class="input-group">
+                                        <span class="input-group-addon input-group-addon-label">State*</span>
+                                        <select id="statesSelection" multiple="multiple" class="form-control">
+                                            <option value='Andaman and Nicobar Islands'>Andaman and Nicobar</option>
+                                            <option value='Andhra Pradesh'>Andhra Pradesh</option>
+                                            <option value='Arunachal Pradesh'>Arunachal Pradesh</option>
+                                            <option value='Assam'>Assam</option>
+                                            <option value='Bihar'>Bihar</option>
+                                            <option value='Chandigarh'>Chandigarh</option>
+                                            <option value='Chhattisgarh'>Chhattisgarh</option>
+                                            <option value='Dadra and Nagar Haveli'>Dadra and Nagar Haveli</option>
+                                            <option value='Daman and Diu'>Daman and Diu</option>
+                                            <option value='Delhi'>Delhi</option>
+                                            <option value='Goa'>Goa</option>
+                                            <option value='Gujarat'>Gujarat</option>
+                                            <option value='Haryana'>Haryana</option>
+                                            <option value='Himachal Pradesh'>Himachal Pradesh</option>
+                                            <option value='Jammu and Kashmir'>Jammu and Kashmir</option>
+                                            <option value='Jharkhand'>Jharkhand</option>
+                                            <option value='Karnataka'>Karnataka</option>
+                                            <option value='Kerala'>Kerala</option>
+                                            <option value='Lakshadweep'>Lakshadweep</option>
+                                            <option value='Madhya Pradesh'>Madhya Pradesh</option>
+                                            <option value='Maharashtra'>Maharashtra</option>
+                                            <option value='Manipur'>Manipur</option>
+                                            <option value='Meghalaya'>Meghalaya</option>
+                                            <option value='Mizoram'>Mizoram</option>
+                                            <option value='Nagaland'>Nagaland</option>
+                                            <option value='Odisha'>Odisha</option>
+                                            <option value='Puducherry'>Puducherry</option>
+                                            <option value='Punjab'>Punjab</option>
+                                            <option value='Rajasthan'>Rajasthan</option>
+                                            <option value='Sikkim'>Sikkim</option>
+                                            <option value='Tamil Nadu'>Tamil Nadu</option>
+                                            <option value='Telengana'>Telengana</option>
+                                            <option value='Tripura'>Tripura</option>
+                                            <option value='Uttar Pradesh'>Uttar Pradesh</option>
+                                            <option value='Uttarakhand'>Uttarakhand</option>
+                                            <option value='West Bengal'>West Bengal</option>
+                                        </select>
+                                    </div>
                             </div>
                         </div>
                     </div>
+                    <div class="row">
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+                            <div class="form-group form-group-margin">
+                                    <div class="input-group">
+                                        <span class="input-group-addon input-group-addon-label">Cities*</span>
+                                        <select id="citiesSelection" multiple="multiple" class="form-control">
+                                            <option value="cheese">Cheese</option>
+                                            <option value="tomatoes">Tomatoes</option>
+                                            <option value="mozarella">Mozzarella</option>
+                                            <option value="mushrooms">Mushrooms</option>
+                                            <option value="pepperoni">Pepperoni</option>
+                                            <option value="onions">Onions</option>
+                                        </select>
+                                    </div>
+                            </div>
+                        </div>
                     </div>
                     <div class="row">
                     <div class="col-md-12 col-sm-12 col-xs-12 margin-left">
