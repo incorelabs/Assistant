374b9624ae6fe82f489d161aeb7006ed844026b6
Kamlesh Bokdia <kbokdia@gmail.com>
2015-10-10 17:14:41 +0530

    Navbar Apis Fixes

--- navbar/LoginDetail.php
+++ navbar/LoginDetail.php
@@ -23,14 +23,10 @@ class LoginDetail
     }
 
     function getDetails(){
-        $sql = "SELECT Table109.RegCode, Table109.FamilyCode, Table109.RegName, Table109.RegEmail, Table109.RegMobile, Table109.PhotoUploaded, Table140.ImagePath FROM Table109 LEFT JOIN Table140 ON Table140.RegCode = Table109.RegCode AND Table140.FamilyCode = Table109.FamilyCode AND Table140.SerialNo = 1 WHERE Table109.RegCode = ".$this->regCode." AND Table109.FamilyCode = ".$this->familyCode;
+        $sql = "SELECT Table109.RegCode, Table109.FamilyCode, Table109.RegName, Table109.RegEmail, Table109.RegMobile, Table109.PhotoUploaded, Table140.ImagePath FROM Table109 LEFT JOIN Table140 ON Table140.RegCode = Table109.RegCode AND Table140.FamilyCode = Table109.FamilyCode AND Table140.SerialNo = 1 WHERE Table109.RegCode = ".$this->regCode." AND Table109.FamilyCode = ".$this->familyCode." LIMIT 1";
 
         if ($result = $this->mysqli->query($sql)) {
-            $i = 0;
-            while ($row = $result->fetch_assoc()) {
-                $this->response[$i] = $row;
-                $i++;
-            }
+            $this->response = $result->fetch_assoc();
         }
         else{
             $this->response = $this->mysqli->error;
